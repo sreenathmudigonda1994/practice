eb1eafc7aa92330e189c691c86fb723acf6a7408
s94 <sreenath.mudigonda1994@gmail.com>
2020-06-03 10:42:06 +0530

    Initial Commit

---
+++ practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  practice
+//
+//  Created by admin on 03/06/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
